5355169c333b2d588113ed4d1b799aa2425c9612
Sergio Luiz Aguirre <seag@demant.com>
2024-09-10 14:47:53 +0200

    add challenges to admin view

--- fitness/admin.py
+++ fitness/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FitnessActivity, UserProfile, FitnessGroup
+from .models import FitnessActivity, UserProfile, FitnessGroup, Challenge
 
 # Register your models here.
 class FitnessActivityAdmin(admin.ModelAdmin):
@@ -21,9 +21,19 @@ class FitnessGroupAdmin(admin.ModelAdmin):
     def get_members(self, obj):
         return ", ".join([member.username for member in obj.members.all()])
     get_members.short_description = 'Members'
+
+class ChallengeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'fitness_group', 'challenge_type', 'target_amount', 'start_date', 'end_date', 'display_users')
+
+    def display_users(self, obj):
+        return ", ".join([user.username for user in obj.users.all()])
+
+    display_users.short_description = 'Users'
+
+
 # Register your models here.
 admin.site.register(FitnessActivity, FitnessActivityAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
-
 # admin.site.register(User, UserAdmin)
 admin.site.register(FitnessGroup,FitnessGroupAdmin)
+admin.site.register(Challenge,ChallengeAdmin)
